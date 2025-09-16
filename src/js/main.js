58d21bddc9101cb47a7a63e4400c95d76a0ff958
EmperorNanayaw <nanayawemperor095@gmail.com>
2025-09-16 20:41:05 +0000

    worked on dynamic pages individual

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
-import { getLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
 
 function updateCartCount() {
   const cart = getLocalStorage("so-cart") || [];
@@ -12,3 +19,4 @@ function updateCartCount() {
 }
 
 updateCartCount();
+productList.init();
