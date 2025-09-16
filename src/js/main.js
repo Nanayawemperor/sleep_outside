af628d58c43450d1ae73ae67bc3301e50e5e2b3b
EmperorNanayaw <nanayawemperor095@gmail.com>
2025-09-16 20:43:54 +0000

    imported getLocalStorage into main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-
+import { getLocalStorage } from "./utils.mjs";
 const dataSource = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
